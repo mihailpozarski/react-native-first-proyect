9e48e4b4db149a77465719afbf89a7b264f29898
Mihail Pozarski <mihailpozarski@outlook.com>
2022-09-28 21:19:43 -0300

    refactor(App): clean up unused code

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, { useState} from 'react'
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  FlatList,
-  TouchableOpacity,
   ActivityIndicator,
+  View,
 } from 'react-native';
+import { styles } from './styles';
+import { colors } from './constants/colors';
 import { DeleteScreen, TasksScreen } from './components/index';
 import { useFonts } from 'expo-font';
 import { TasksContextProvider } from './context/TasksContext';
@@ -17,11 +14,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [selectedTask, setSelectedTask] = useState(null);
-  const [selectedTasks, setSelectedTasks] = useState([]);
   const [loaded] = useFonts({
     'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
     'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
@@ -38,61 +30,7 @@ export default function App() {
     );
   }
 
-  const onHandleChangeText = (text) => {
-    setTask(text);
-  }
-
-  const onHandleModal = (id) => {
-    const task = tasks.find((task) => task.id === id);
-    setSelectedTask(task);
-    setModalVisible(true);
-  }
-
-  const selectTask = (item) => {
-    if (selectedTasks.includes(item)) {
-      setSelectedTasks(selectedTasks.filter((selected) => selected.id !== item.id));
-    } else {
-      setSelectedTasks([...selectedTasks, item]);
-    }
-  }
-
-  const addItem = () => {
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      { id: Date.now(), value: task },
-    ]);
-    setTask('');
-  }
-
-  const onHandleDeleteItem = (id) => {
-    setTasks(tasks.filter((item) => item.id !== id));
-    setSelectedTasks(selectedTasks.filter((item) => item.id !== id));
-    setSelectedTask(null);
-    setModalVisible(!modalVisible);
-  }
-
-  const onHandleCancel = () => {
-    setSelectedTask(null);
-    setModalVisible(!modalVisible);
-  }
-
-  const deleteSelectedTasks = () => {
-    setTasks(tasks.filter((item) => !selectedTasks.includes(item)));
-    setSelectedTasks([]);
-  }
-
-  const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <CheckBox value={selectedTasks.includes(item)} onValueChange={() => selectTask(item)} />
-      <Text style={styles.item}>{item.value}</Text>
-      <TouchableOpacity style={styles.button} onPress={() => onHandleModal(item.id)}>
-        <Text style={styles.delete}>X</Text>
-      </TouchableOpacity>
-    </View>
-  )
-
-  if (!!selectedTask){
-    return (
+  return (
     <NavigationContainer>
       <TasksContextProvider>
         <Stack.Navigator>
@@ -101,6 +39,5 @@ export default function App() {
         </Stack.Navigator>
       </TasksContextProvider>
     </NavigationContainer>
-    );
-  }
+  );
 }
