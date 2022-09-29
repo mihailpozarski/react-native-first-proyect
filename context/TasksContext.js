02ee30d765f65ad3fb144a9baf81aa6e67700cdd
Mihail Pozarski <mihailpozarski@outlook.com>
2022-09-28 21:06:42 -0300

    feat(TasksContext): create context to handle shared states

---
+++ context/TasksContext.js
@@ -0,0 +1,53 @@
+import React, {createContext, useState}  from 'react';
+
+export const TasksContext = createContext([]);
+
+export const TasksContextProvider = ({children}) => {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [selectedTasks, setSelectedTasks] = useState([]);
+
+  const addTask = () => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), value: task },
+    ]);
+    setTask('');
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((item) => item.id !== id));
+    setSelectedTasks(selectedTasks.filter((item) => item.id !== id));
+  };
+
+  const addTaskToSelectedTasks = (item) => {
+    if (selectedTasks.includes(item)) {
+      setSelectedTasks(selectedTasks.filter((selected) => selected.id !== item.id));
+    } else {
+      setSelectedTasks([...selectedTasks, item]);
+    }
+  };
+
+  const deleteAllSelectedTasks = () => {
+    setTasks(tasks.filter((item) => !selectedTasks.includes(item)));
+    setSelectedTasks([]);
+  };
+
+  return (
+    <TasksContext.Provider
+      value={{
+        task,
+        tasks,
+        selectedTasks,
+        setTask,
+        setTasks,
+        setSelectedTasks,
+        addTask,
+        deleteTask,
+        addTaskToSelectedTasks,
+        deleteAllSelectedTasks,
+      }}>
+      {children}
+    </TasksContext.Provider>
+  );
+}
