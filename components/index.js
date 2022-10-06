2dfd5ed3580ace2f789feca786f3a0132f39bc0d
Mihail Pozarski <mihailpozarski@outlook.com>
2022-10-05 21:08:07 -0300

    feat(components): export new components

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { DeleteScreen } from './delete-screen/index';
+export { DeleteAllScreen } from './delete-all-screen/index';
+export { default as TasksNavigation } from './tasks-navigation/index';
 export { default as AddTask } from './add-task/index';
 export { default as TasksScreen } from './tasks-screen/index';
