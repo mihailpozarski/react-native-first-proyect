d9427f5ab26e2780ecccd48a6ef1ae5932364de2
Mihail Pozarski <mihailpozarski@outlook.com>
2022-10-05 21:05:36 -0300

    feat(Colors): add useful colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,4 +1,10 @@
 export const colors = {
   itemBackground: '#9F84BD',
   background: '#F5F5F5',
-}
+  primary: '#F5F5F5',
+  secondary: '#cccccc',
+  warning: '#FF0000',
+  success: '#00FF00',
+  text: '#000000',
+  textSecondary: '#ffffff',
+};
