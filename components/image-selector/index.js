2d1894e8176c94d84bab29463b22be4cebb37a53
Mihail Pozarski <mihailpozarski@outlook.com>
2022-10-24 21:55:34 -0300

    feat(ImageSelector): add image selector

---
+++ components/image-selector/index.js
@@ -0,0 +1,45 @@
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+import { View, Image, Text, Alert, Button } from "react-native";
+import { styles } from "./styles";
+
+const ImageSelector = ({ onImage }) => {
+  const [pickedUrl, setPickedUrl] = useState(null);
+
+  const verifyPermissions = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert("Permiso denegado", "Necesitamos permisos para usar la cámara", [{ text: "Ok" }]);
+      return false;
+    }
+    return true;
+  };
+
+  const onHandleTakeImage = async () => {
+    const isCameraPermission = await verifyPermissions();
+    if (!isCameraPermission) return;
+
+    const image = await ImagePicker.launchCameraAsync({
+      aspect: [16, 9],
+      quality: 0.7,
+    });
+
+    setPickedUrl(image.uri);
+    onImage(image.uri);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.preview}>
+        {!pickedUrl ? (
+          <Text>No hay imagen seleccionada</Text>
+        ) : (
+          <Image style={styles.image} source={{ uri: pickedUrl }} />
+        )}
+      </View>
+      <Button title="Tomar foto" onPress={onHandleTakeImage} />
+    </View>
+  );
+};
+
+export default ImageSelector;
---
+++ components/image-selector/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+  container: {},
+  preview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: colors.primary,
+    borderWidth: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
